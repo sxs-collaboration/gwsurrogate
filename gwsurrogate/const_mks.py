eef35948a776b06551f99916137c15cbade950f8
Scott Field <sfield@umd.edu>
2014-01-26 00:28:22 -0500

    mks constants

---
+++ gwsurrogate/const_mks.py
@@ -0,0 +1,7 @@
+c         = 2.99792458e8     # Speed of light (MKS)
+G         = 6.67428e-11      # Gravitation constant (MKS)
+Msun      = 1.98892e30       # Solar mass (kg)
+Msuninsec = Msun * G / c**3  # Solar mass (secs)
+Mpcinm    = 3.08568025e22    # Megaparsecs (m)
+Mpcinkm   = Mpcinm/1000.0    # Megaparsecs (km)
+
