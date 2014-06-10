9fd74713f062bc00c41e218d8adafce36830c653
Scott Field <sfield@umd.edu>
2014-06-10 13:08:27 -0400

    simplified setup script and tweaks to readme

--- setup.py
+++ setup.py
@@ -1,22 +1,6 @@
 from distutils.core import setup
 import sys
 
-
-# Encoding
-major, minor1, minor2, release, serial = sys.version_info
-if major >= 3:
-    def rd(filename):
-        f = open(filename, encoding="utf-8")
-        r = f.read()
-        f.close()
-        return r
-else:
-    def rd(filename):
-        f = open(filename)
-        r = f.read()
-        f.close()
-        return r
-
 setup(name='gwsurrogate',
       version='0.1',
       author='Scott Field, Chad Galley',
@@ -24,7 +8,6 @@ setup(name='gwsurrogate',
       packages=['gwsurrogate'],
       license='GPL',
       description='an easy to use interface to gravitational wave surrogate models',
-      long_description="\n\n"+rd("README"),
       install_requires=["numpy","matplotlib","h5py"],
       classifiers=[
                 'Intended Audience :: Other Audience',
@@ -35,7 +18,5 @@ setup(name='gwsurrogate',
                 'Topic :: Scientific/Engineering',
                 'Topic :: Scientific/Engineering :: Mathematics',
                 'Topic :: Scientific/Engineering :: Physics',
-                ],
-
-      )
-
+      ],
+)
