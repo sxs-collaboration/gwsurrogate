62e268694ee1c2f987d95d1a27101b2ca4f8f466
Chad Galley <crgalley@tapir.caltech.edu>
2014-12-12 10:58:03 -0800

    Adding H5Surrogate stuff to import statements.

--- gwsurrogate/surrogate.py
+++ gwsurrogate/surrogate.py
@@ -36,7 +36,7 @@ import matplotlib.pyplot as plt
 import time
 import os as os
 from parametric_funcs import function_dict as my_funcs
-from surrogateIO import TextSurrogateRead, TextSurrogateWrite
+from surrogateIO import H5SurrogateRead, H5SurrogateWrite, TextSurrogateRead, TextSurrogateWrite
 
 try:
 	import h5py
