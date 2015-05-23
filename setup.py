d15a42021e9a8507006e5767680f62c19f74a087
Scott Field <sfield@umd.edu>
2015-05-23 19:55:39 -0400

    edits for distutils

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from distutils.core import setup
 import sys
 
 setup(name='gwsurrogate',
-      version='0.2.1',
+      version='1.0',
       author='Scott Field, Chad Galley',
       author_email='sfield@astro.cornell.edu',
       packages=['gwsurrogate'],
       license='MIT',
       contributors=[
-      # Alphabetical by first name.
+      # Alphabetical by last name.
       "Jonathan Blackman"],
       description='an easy to use interface to gravitational wave surrogate models',
       install_requires=["numpy","matplotlib","scipy"],
