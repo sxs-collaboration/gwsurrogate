2742b7d7db3b0137b3c6456575e72fbc539479cf
Scott Field <sfield@umd.edu>
2015-05-23 21:24:58 -0400

    verion 0.3

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 import sys
 
 setup(name='gwsurrogate',
-      version='1.0',
+      version='0.3',
       author='Scott Field, Chad Galley',
       author_email='sfield@astro.cornell.edu',
       packages=['gwsurrogate'],
