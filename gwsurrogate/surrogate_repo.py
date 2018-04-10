270bf3458acf5ac14e96bc525b1e0fdab214cb6a
Scott Field <sfield@umassd.edu>
2018-04-10 00:40:50 -0400

    add surrogates to repo world

--- gwsurrogate/surrogate_repo.py
+++ gwsurrogate/surrogate_repo.py
@@ -38,21 +38,33 @@ surrogate_info = namedtuple('surrogate_info', ['url', 'desc'])
 _surrogate_world = {}
 
 _surrogate_world['EOBNRv2'] = \
-  surrogate_info('https://www.dropbox.com/s/uyliuy37uczu3ug/EOBNRv2.tar.gz',\
-                 '''(WARNING: older surrogate may not evaluate with 
-most recent version of gwsurrogate) Collection of single mode surrogates 
-from mass ratios 1 to 10, as long as 190000M and modes (2,1), (2,2), (3,3), 
-(4,4), (5,5). This is not a true multi-mode surrogate, and relative time/phase
-information between the modes have not been preseved. For more information see 
-http://journals.aps.org/prx/abstract/10.1103/PhysRevX.4.031006''')
+  surrogate_info('https://www.dropbox.com/s/uyliuy37uczu3ug/EOBNRv2.tar.gz',
+                 '''(WARNING: older surrogate may not evaluate with most recent version of gwsurrogate) 
+                    Collection of single mode surrogates from mass ratios 1 to 10, as long as 190000M 
+                    and modes (2,1), (2,2), (3,3), (4,4), (5,5). This is not a true multi-mode surrogate, 
+                    and relative time/phase information between the modes have not been preserved. 
+                    For more information see http://journals.aps.org/prx/abstract/10.1103/PhysRevX.4.031006''')
 
 _surrogate_world['SpEC_q1_10_NoSpin'] = \
-  surrogate_info('https://www.black-holes.org/surrogates/data/SpEC_q1_10_NoSpin_nu5thDegPoly_exclude_2_0.h5',\
-                 '''A multimode surrogate model built from numerical relativity
-simulations performed with SpEC. The surrogate covers mass ratios from 1 to 10,
-durations corresponding to about 15 orbits before merger, and many harmonic
-modes. For more information see https://www.black-holes.org/surrogates/ or
-http://arxiv.org/abs/1502.07758''')
+  surrogate_info('https://www.black-holes.org/surrogates/data/SpEC_q1_10_NoSpin_nu5thDegPoly_exclude_2_0.h5',
+                 '''A multimode surrogate model built from numerical relativity simulations performed with SpEC.
+                 The surrogate covers mass ratios from 1 to 10, durations corresponding to about 15 orbits before
+                merger, and many harmonic modes. For more information see 
+                https://www.black-holes.org/surrogates/ or http://arxiv.org/abs/1502.07758''')
+
+_surrogate_world['SpEC_q1_10_NoSpin_linear'] = \
+  surrogate_info('XXX.h5',
+                 '''Linear surrogate version of the SpEC_q1_10_NoSpin model using fast splines.
+                 This surrogate is designed to be loaded with the original gws interface. 
+                 See http://iopscience.iop.org/article/10.1088/1361-6382/aa7649/meta                  
+                  ''')
+
+_surrogate_world['SpEC_q1_10_NoSpin_linear_alt'] = \
+  surrogate_info('XXX.h5',
+                 '''Linear surrogate version of the SpEC_q1_10_NoSpin model using fast splines.
+                 This surrogate is designed to be loaded with an alternative (experimental) gws interface. 
+                 See http://iopscience.iop.org/article/10.1088/1361-6382/aa7649/meta                  
+                  ''')
 
 def download_path():
   '''return the default path for downloaded surrogates'''
@@ -63,7 +75,7 @@ def download_path():
   return gws_path+'/../surrogate_downloads/'
 
 def list():
-  '''show all known surrogates available for download'''
+  """show all known surrogates available for download"""
 
   for surr_key in _surrogate_world.keys():
     print(surr_key+'...')
@@ -71,7 +83,7 @@ def list():
     print("  Description: "+_surrogate_world[surr_key].desc+'\n\n')
 
 def _unzip(surr_name,sdir=download_path()):
-  '''unzip a tar.gz surrogate and remove the tar.gz file'''
+  """unzip a tar.gz surrogate and remove the tar.gz file"""
 
   os.chdir(sdir)
   os.system('tar -xvzf '+surr_name)
@@ -80,9 +92,9 @@ def _unzip(surr_name,sdir=download_path()):
   return sdir+surr_name.split('.')[0]
 
 def get(surr_name,sdir=download_path()):
-  '''pass a valid surr_name from the repo list and download location sdir. 
+  """pass a valid surr_name from the repo list and download location sdir.
      The default path is used if no location supplied. tar.gz surrogates
-     are automatically unziped. The new surrogate path is returned.'''
+     are automatically unziped. The new surrogate path is returned."""
 
   if surr_name in _surrogate_world:
     surr_url = _surrogate_world[surr_name].url
