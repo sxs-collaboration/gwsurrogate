7516675d08ce9aa39e2bf9493ba3dce74d841006
Vijay Varma <vvarma@caltech.edu>
2019-05-01 21:06:52 -0700

    Get version, licence, etc from gwsurrogate/surrogate.py
    This way we don't have to repeat ourselves.
    Also gwsurrogate.__version__ was not available before.

--- gwsurrogate/__init__.py
+++ gwsurrogate/__init__.py
@@ -70,29 +70,13 @@ of interest.
 
 """
 
-__copyright__ = "Copyright (C) 2014 Scott Field, Chad Galley"
-__email__ = "sfield@umassd.edu"
-__status__ = "testing"
-__author__ = "Jonathan Blackman, Scott Field, Chad Galley, Vijay Varma"
-__license__ = """
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in
-all copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-THE SOFTWARE.
-"""
+from . import surrogate
+__author__ = surrogate.__author__
+__email__ = surrogate.__email__
+__copyright__ = surrogate.__copyright__
+__license__ = surrogate.__license__
+__version__ = surrogate.__version__
+__doc__ = surrogate.__doc__
 
 from .surrogate import *
 from . import catalog
