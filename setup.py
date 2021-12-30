d62a41219cc0faf400b14c8096e929de96065f64
Leo C. Stein <leo.stein@gmail.com>
2021-12-30 11:15:14 -0600

    Subclassing workaround to numpy build order issue
    Thanks to https://stackoverflow.com/a/42163080/1695428 for the solution

--- setup.py
+++ setup.py
@@ -36,13 +36,28 @@ extmod = Extension('gwsurrogate.spline_interp_Cwrapper._spline_interp',
 extmods.append(extmod)
 
 # build extension 2: precessing utils
-import numpy
 extmod =  Extension('gwsurrogate.precessing_utils._utils',
                     sources=['gwsurrogate/precessing_utils/src/precessing_utils.c'],
-                    include_dirs = ['gwsurrogate/precessing_utils/include', numpy.get_include()],
+                    include_dirs = ['gwsurrogate/precessing_utils/include'],
                     language='c',
                     extra_compile_args = ['-std=c99','-fPIC', '-O3'])
 extmods.append(extmod)
+
+# Workaround: Only import numpy once reqs have been imported
+# Thanks to https://stackoverflow.com/a/42163080/1695428
+from distutils.command.build_ext import build_ext
+class LateNumpyIncludeCommand(build_ext):
+    """build_ext command for use when numpy headers are needed."""
+    def run(self):
+
+        # Import numpy here, only when headers are needed
+        import numpy
+
+        # Add numpy headers to include_dirs
+        self.include_dirs.append(numpy.get_include())
+
+        # Call original build_ext command
+        build_ext.run(self)
 
 # Extract code version from surrogate.py
 def read_main_file(key):
@@ -82,5 +97,6 @@ setup(name='gwsurrogate',
                 'Topic :: Scientific/Engineering :: Mathematics',
                 'Topic :: Scientific/Engineering :: Physics',
       ],
+      cmdclass = {'build_ext': LateNumpyIncludeCommand},
       ext_modules = extmods,
 )
