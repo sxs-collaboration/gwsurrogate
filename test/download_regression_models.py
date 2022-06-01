3a502426c4b37abff208ecb9dc968f44a1d56064
Scott Field <sfield@umassd.edu>
2022-05-31 22:52:27 -0400

    Check downloaded models hash is correct

--- test/download_regression_models.py
+++ test/download_regression_models.py
@@ -2,8 +2,18 @@
 
 This is useful to do when using continuous integration. """
 
-
 import gwsurrogate as gws
+import hashlib, os
+
+
+def md5(fname):
+  """ Compute has from file. code taken from
+  https://stackoverflow.com/questions/3431825/generating-an-md5-checksum-of-a-file"""
+  hash_md5 = hashlib.md5()
+  with open(fname, "rb") as f:
+    for chunk in iter(lambda: f.read(4096), b""):
+      hash_md5.update(chunk)
+  return hash_md5.hexdigest()
 
 models = ['SpEC_q1_10_NoSpin_linear_alt',
 'NRHybSur3dq8',
@@ -15,3 +25,9 @@ models = ['SpEC_q1_10_NoSpin_linear_alt',
 
 for model in models:
   gws.catalog.pull(model)
+  surr_url = gws.catalog._surrogate_world[model].url
+  path_to_model = gws.catalog.download_path()+os.path.basename(surr_url)
+  print("md5 Hash of %s is %s"%(model,md5(path_to_model)))
+  if not gws.catalog.is_file_recent(path_to_model):
+     print("File download failed!") 
+     assert(False)
